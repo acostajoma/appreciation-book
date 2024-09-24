96e732a0bb692d61a9612fc2ad0e20f764f6b7e7
Jose Martín Acosta <87622709+acostajoma@users.noreply.github.com>
2024-09-24 08:38:39 -0600

    Adding needed packages and configuring cloudflare

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,7 +6,14 @@ declare global {
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
-		// interface Platform {}
+		interface Platform {
+			env: {
+				DB: D1Database;
+			};
+			cf: CfProperties;
+			ctx: ExecutionContext;
+			caches: (CacheStorage & { default: Cache }) | CacheStorage_2;
+		}
 	}
 }
 
